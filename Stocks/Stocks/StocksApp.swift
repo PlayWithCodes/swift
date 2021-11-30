03f841d7adacd05354abcc85fd8af311b8b1b196
turtle <michael.app.space@gmail.com>
2021-11-30 11:40:40 +0900

    feat: Create index.html

---
+++ Stocks/Stocks/StocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksApp.swift
+//  Stocks
+//
+//  Created by grade8 on 2021/11/30.
+//
+
+import SwiftUI
+
+@main
+struct StocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
