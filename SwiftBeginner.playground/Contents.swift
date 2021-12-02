68b2dacba3b65ffef8fc590ca643b1a0aaf745b4
turtle <michael.app.space@gmail.com>
2021-12-03 04:54:04 +0900

    first commit

---
+++ SwiftBeginner.playground/Contents.swift
@@ -0,0 +1,22 @@
+import Cocoa
+
+let filename = "paris.jpg"
+let star = "⭐️"
+let result = "⭐️ You win! ⭐️"
+let quote = "Then he tapped a sign saying \"Believe\" and walked away."
+
+let movie = """
+A day in
+the life of an
+Apple engineer
+"""
+
+print(star.count)
+print(result.uppercased())
+print(filename.hasSuffix(".jpg"))
+print(movie.hasPrefix("A day"))
+
+
+//
+
+
