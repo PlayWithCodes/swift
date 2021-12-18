808c21240db8daea2350c7b9458613435d7419de
turtle <michael.app.space@gmail.com>
2021-12-18 22:35:55 +0900

    WeSplit Wrap up

---
+++ SwiftUI/WeSplitReview/WeSplitReview/WeSplitReviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitReviewApp.swift
+//  WeSplitReview
+//
+//  Created by grade8 on 2021/12/18.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitReviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
