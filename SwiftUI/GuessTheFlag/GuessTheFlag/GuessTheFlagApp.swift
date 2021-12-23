ccf0d81c883fbef57421504696fd4a81d2ae8db0
turtle <michael.app.space@gmail.com>
2021-12-23 20:52:02 +0900

    first commit

---
+++ SwiftUI/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by grade8 on 2021/12/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
