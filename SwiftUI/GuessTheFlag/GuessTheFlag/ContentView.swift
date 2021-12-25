420a5b349709d131d0d17a8f469a6a93611fa8cb
turtle <michael.app.space@gmail.com>
2021-12-25 20:52:54 +0900

    feat: Try Buttons

--- SwiftUI/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ SwiftUI/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -1,20 +1,32 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-//      LinearGradient(gradient: Gradient(colors: [.white, .black]), startPoint: .top, endPoint: .bottom)
-      
-//      LinearGradient(gradient: Gradient(stops: [
-//        .init(color: .white, location: 0.45),
-//        .init(color: .black, location: 0.55)
-//      ]), startPoint: .top, endPoint: .bottom)
-      
-      AngularGradient(colors: [.red, .orange, .yellow, .green, .blue, .purple], center: .center)
+  var body: some View {
+//    VStack {
+//      Button("Button 1") { }
+//      .buttonStyle(.bordered)
+//      Button("Button 2", role: .destructive) { }
+//      .buttonStyle(.bordered)
+//      Button("Button 3") { }
+//      .buttonStyle(.borderedProminent)
+//      .tint(.mint)
+//      Button("Button 4") { }
+//      .buttonStyle(.borderedProminent)
+//    }
+    
+    Button {
+      print("Button was tapped")
+    } label: {
+      Text("Tap me!")
+        .padding()
+        .foregroundColor(.white)
+        .background(.red)
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
