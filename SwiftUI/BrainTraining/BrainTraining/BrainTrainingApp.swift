46f9c8ad1ae431ab28ad64d72005310b171dfd01
turtle <michael.app.space@gmail.com>
2022-01-13 17:03:53 +0900

    first commit

---
+++ SwiftUI/BrainTraining/BrainTraining/BrainTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainTrainingApp.swift
+//  BrainTraining
+//
+//  Created by grade8 on 2022/01/13.
+//
+
+import SwiftUI
+
+@main
+struct BrainTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
