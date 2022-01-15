5114e4b39e6f6ebef08a7de5fc71a3a5632a3030
turtle <michael.app.space@gmail.com>
2022-01-15 11:53:19 +0900

    feat: Alternate between prompting the player to win or lose.

--- SwiftUI/BrainTraining/BrainTraining/ContentView.swift
+++ SwiftUI/BrainTraining/BrainTraining/ContentView.swift
@@ -2,18 +2,40 @@ import SwiftUI
 
 struct ContentView: View {
   private var rpcArr = ["Rock", "Paper", "Scissors"]
-  @State private var rpc = "Tap the play!!"
+  @State private var rpc = "Tap a button"
   
   var body: some View {
     VStack {
       Text(rpc)
-      Button("Play") {
-        rpc = rpcArr[Int.random(in: 0...2)]
+      
+      HStack {
+        Button("Rock") {
+          let randomRpc = rpcArr[Int.random(in: 0...2)]
+          rpc = randomRpc == rpcArr[1] ? "\(randomRpc). You lose" : "\(randomRpc). You win"
+        }
+        .padding()
+        .background(.blue)
+        .foregroundColor(.white)
+        .clipShape(Capsule())
+        
+        Button("Paper") {
+          let randomRpc = rpcArr[Int.random(in: 0...2)]
+          rpc = randomRpc == rpcArr[2] ? "\(randomRpc). You lose" : "\(randomRpc). You win"
+        }
+        .padding()
+        .background(.blue)
+        .foregroundColor(.white)
+        .clipShape(Capsule())
+        
+        Button("Scissors") {
+          let randomRpc = rpcArr[Int.random(in: 0...2)]
+          rpc = randomRpc == rpcArr[0] ? "\(randomRpc). You lose" : "\(randomRpc). You win"
+        }
+        .padding()
+        .background(.blue)
+        .foregroundColor(.white)
+        .clipShape(Capsule())
       }
-      .padding()
-      .background(.blue)
-      .foregroundColor(.white)
-      .clipShape(Capsule())
     }
   }
 }
