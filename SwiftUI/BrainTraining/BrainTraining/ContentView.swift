ad0c80da3949bdb34f4fe580f0854c3ff5c67fd3
turtle <michael.app.space@gmail.com>
2022-01-17 07:16:19 +0900

    feat: Ends after 10 questions, at which point their score is shown

--- SwiftUI/BrainTraining/BrainTraining/ContentView.swift
+++ SwiftUI/BrainTraining/BrainTraining/ContentView.swift
@@ -5,6 +5,8 @@ struct ContentView: View {
   @State private var rpc = "Tap a button"
   @State private var score = 0
   @State private var isWin = false
+  @State private var round = 0
+  @State private var showingAlert = false
   
   var body: some View {
     VStack {
@@ -12,6 +14,15 @@ struct ContentView: View {
       
       HStack {
         Button("Rock") {
+          round += 1
+          
+          if round == 10 {
+            showingAlert = true
+            return
+          }
+          
+          showingAlert = false
+          
           let randomRpc = rpcArr[Int.random(in: 0...2)]
           isWin = randomRpc == rpcArr[1] ? false : true
           if isWin {
@@ -19,14 +30,30 @@ struct ContentView: View {
             score += 10
           } else {
             rpc = "\(randomRpc). You lose"
+            score -= 10
           }
         }
         .padding()
         .background(.blue)
         .foregroundColor(.white)
         .clipShape(Capsule())
+        .alert("Final score: \(score)", isPresented: $showingAlert) {
+          Button("OK", role: .cancel) {
+            round = 0
+            score = 0
+          }
+        }
         
         Button("Paper") {
+          round += 1
+          
+          if round == 10 {
+            showingAlert = true
+            return
+          }
+          
+          showingAlert = false
+          
           let randomRpc = rpcArr[Int.random(in: 0...2)]
           isWin = randomRpc == rpcArr[2] ? false : true
           if isWin {
@@ -34,14 +61,30 @@ struct ContentView: View {
             score += 10
           } else {
             rpc = "\(randomRpc). You lose"
+            score -= 10
           }
         }
         .padding()
         .background(.blue)
         .foregroundColor(.white)
         .clipShape(Capsule())
+        .alert("Final score: \(score)", isPresented: $showingAlert) {
+          Button("OK", role: .cancel) {
+            round = 0
+            score = 0
+          }
+        }
         
         Button("Scissors") {
+          round += 1
+          
+          if round == 10 {
+            showingAlert = true
+            return
+          }
+          
+          showingAlert = false
+          
           let randomRpc = rpcArr[Int.random(in: 0...2)]
           isWin = randomRpc == rpcArr[0] ? false : true
           if isWin {
@@ -49,15 +92,20 @@ struct ContentView: View {
             score += 10
           } else {
             rpc = "\(randomRpc). You lose"
+            score -= 10
           }
         }
         .padding()
         .background(.blue)
         .foregroundColor(.white)
         .clipShape(Capsule())
+        .alert("Final score: \(score)", isPresented: $showingAlert) {
+          Button("OK", role: .cancel) {
+            round = 0
+            score = 0
+          }
+        }
       }
-      
-      Text("Score: \(score)")
     }
   }
 }
