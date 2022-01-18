71078b4827e4416705b8776016efddd0b0d47868
turtle <michael.app.space@gmail.com>
2022-01-18 21:36:05 +0900

    refactor: Make a custom button view

--- SwiftUI/BrainTraining/BrainTraining/ContentView.swift
+++ SwiftUI/BrainTraining/BrainTraining/ContentView.swift
@@ -1,110 +1,119 @@
 import SwiftUI
 
-struct ContentView: View {
-  private var rpcArr = ["Rock", "Paper", "Scissors"]
-  @State private var rpc = "Tap a button"
-  @State private var score = 0
-  @State private var isWin = false
-  @State private var round = 0
+enum RockPaperScissors {
+  case rock
+  case paper
+  case scissors
+}
+
+struct GameMachine {
+  static var count = 0
+  static var score = 0
+  @State static var gameResult = "Tap a button"
+  
+  static func countUp() {
+    count += 1
+  }
+  
+  static func scoreUp() {
+    score += 10
+  }
+  
+  static func scoreDown() {
+    score -= 10
+  }
+  
+  static func runRpc(userChoice: RockPaperScissors) {
+    let computerRpc = Int.random(in: 0...2)
+    var computerChoice = RockPaperScissors.rock
+    
+    countUp()
+    
+    switch computerRpc {
+      case 1:
+        computerChoice = RockPaperScissors.paper
+      case 2:
+        computerChoice = RockPaperScissors.scissors
+      default:
+        computerChoice = RockPaperScissors.rock
+    }
+    
+    if computerChoice == userChoice {
+      gameResult = "draw"
+      return
+    }
+    
+    switch userChoice {
+      case RockPaperScissors.rock:
+        if computerChoice == RockPaperScissors.scissors {
+          gameResult = "You win"
+          GameMachine.scoreUp()
+        } else {
+          gameResult = "You lose"
+          GameMachine.scoreDown()
+        }
+      case RockPaperScissors.paper:
+        if computerChoice == RockPaperScissors.rock {
+          gameResult = "You win"
+          GameMachine.scoreUp()
+        } else {
+          gameResult = "You lose"
+          GameMachine.scoreDown()
+        }
+      case RockPaperScissors.scissors:
+        if computerChoice == RockPaperScissors.paper {
+          gameResult = "You win"
+          GameMachine.scoreUp()
+        } else {
+          gameResult = "You lose"
+          GameMachine.scoreDown()
+        }
+    }
+  }
+  
+  static func endGame() -> Bool {
+    return count == 10 ? true : false
+  }
+  
+  static func reset() {
+    count = 0
+    score = 0
+  }
+}
+
+struct RpcButton: View {
+  var buttonName: String
+  var userChoice: RockPaperScissors
   @State private var showingAlert = false
   
   var body: some View {
+    Button(buttonName) {
+      GameMachine.runRpc(userChoice: userChoice)
+      showingAlert = GameMachine.endGame()
+    }
+    .padding()
+    .background(.blue)
+    .foregroundColor(.white)
+    .clipShape(Capsule())
+    .alert("Final score: \(GameMachine.score)", isPresented: $showingAlert) {
+      Button("OK", role: .cancel) {
+        GameMachine.reset()
+      }
+    }
+  }
+}
+
+struct ContentView: View {
+  var body: some View {
     VStack {
-      Text(rpc)
+      Text(GameMachine.gameResult)
       
       HStack {
-        Button("Rock") {
-          round += 1
-          
-          if round == 10 {
-            showingAlert = true
-            return
-          }
-          
-          showingAlert = false
-          
-          let randomRpc = rpcArr[Int.random(in: 0...2)]
-          isWin = randomRpc == rpcArr[1] ? false : true
-          if isWin {
-            rpc = "\(randomRpc). You win"
-            score += 10
-          } else {
-            rpc = "\(randomRpc). You lose"
-            score -= 10
-          }
-        }
-        .padding()
-        .background(.blue)
-        .foregroundColor(.white)
-        .clipShape(Capsule())
-        .alert("Final score: \(score)", isPresented: $showingAlert) {
-          Button("OK", role: .cancel) {
-            round = 0
-            score = 0
-          }
-        }
+        RpcButton(buttonName: "Rock", userChoice: RockPaperScissors.rock)
         
-        Button("Paper") {
-          round += 1
-          
-          if round == 10 {
-            showingAlert = true
-            return
-          }
-          
-          showingAlert = false
-          
-          let randomRpc = rpcArr[Int.random(in: 0...2)]
-          isWin = randomRpc == rpcArr[2] ? false : true
-          if isWin {
-            rpc = "\(randomRpc). You win"
-            score += 10
-          } else {
-            rpc = "\(randomRpc). You lose"
-            score -= 10
-          }
-        }
-        .padding()
-        .background(.blue)
-        .foregroundColor(.white)
-        .clipShape(Capsule())
-        .alert("Final score: \(score)", isPresented: $showingAlert) {
-          Button("OK", role: .cancel) {
-            round = 0
-            score = 0
-          }
-        }
+        RpcButton(buttonName: "Paper", userChoice: RockPaperScissors.paper)
         
-        Button("Scissors") {
-          round += 1
-          
-          if round == 10 {
-            showingAlert = true
-            return
-          }
-          
-          showingAlert = false
-          
-          let randomRpc = rpcArr[Int.random(in: 0...2)]
-          isWin = randomRpc == rpcArr[0] ? false : true
-          if isWin {
-            rpc = "\(randomRpc). You win"
-            score += 10
-          } else {
-            rpc = "\(randomRpc). You lose"
-            score -= 10
-          }
-        }
-        .padding()
-        .background(.blue)
-        .foregroundColor(.white)
-        .clipShape(Capsule())
-        .alert("Final score: \(score)", isPresented: $showingAlert) {
-          Button("OK", role: .cancel) {
-            round = 0
-            score = 0
-          }
-        }
+        RpcButton(buttonName: "Scissors", userChoice: RockPaperScissors.scissors)
       }
     }
   }
