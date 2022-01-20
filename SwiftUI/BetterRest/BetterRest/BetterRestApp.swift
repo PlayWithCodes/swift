afbce11d4297a28cec7fa26ca49962c4ad6e8bf9
turtle <michael.app.space@gmail.com>
2022-01-20 21:13:42 +0900

    first commit

---
+++ SwiftUI/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by grade8 on 2022/01/20.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
