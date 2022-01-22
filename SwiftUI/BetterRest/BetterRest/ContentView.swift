db72de559d87fb08aa635d4c0ca5b7d7c966540a
turtle <michael.app.space@gmail.com>
2022-01-22 16:25:25 +0900

    test: Try Datepicker

--- SwiftUI/BetterRest/BetterRest/ContentView.swift
+++ SwiftUI/BetterRest/BetterRest/ContentView.swift
@@ -1,14 +1,17 @@
 import SwiftUI
 
 struct ContentView: View {
-  @State private var sleepAmount = 8.0
-    var body: some View {
-      Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
-    }
+  @State private var wakeUp = Date.now
+  
+  var body: some View {
+    DatePicker("Please enter a time", selection: $wakeUp, in: Date.now...)
+    //DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+      //.labelsHidden()
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
