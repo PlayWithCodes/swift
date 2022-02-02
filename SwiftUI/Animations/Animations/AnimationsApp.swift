c4624c9f6e8901e3589db02b9a060e93782d6e28
turtle <michael.app.space@gmail.com>
2022-02-02 21:11:03 +0900

    test: Try animation

---
+++ SwiftUI/Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by grade8 on 2022/02/02.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI/Animations/Animations/ContentView.swift
@@ -0,0 +1,24 @@
+import SwiftUI
+
+struct ContentView: View {
+  @State private var animationAmount = 1.0
+  
+  var body: some View {
+    Button("Tap Me") {
+      animationAmount += 1
+    }
+    .padding(50)
+    .background(.green)
+    .foregroundColor(.white)
+    .clipShape(Circle())
+    .scaleEffect(animationAmount)
+    .blur(radius: (animationAmount - 1) * 3)
+    .animation(.default, value: animationAmount)
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
